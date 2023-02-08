b494a097193e8993339ce656bf78d4e978c076bd
Pass1-2-3 <118269637+Pass1-2-3@users.noreply.github.com>
2023-02-08 18:07:13 +0530

    Create REVTEST02.java

---
+++ src/test/java/REVTEST02.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import com.example.javamavenjunithelloworld.Hello;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+@Test
+@Test
+ * Unit test for Hello.
+ * <p/>
+ * A unit test aims to test all code and code paths of a specific class.
+ */
+//@Test
+//@Test
+public class REVTEST02 {
+
+    @Test
+    public void FILE_02_TEST_01() {
+        Hello WORLD = new Hello();
+        int SUM = WORLD.evaluate("3+3+3");
+        assertEquals(9, SUM);
+    }
+  
+  @Test
+    public void FILE_02_TEST_02() {
+        Hello WORLD = new Hello();
+        int SUM = WORLD.evaluate("4+5");
+        assertEquals(9, SUM);
+    }
+    
+}
