ca4d2e98aa81cd902afafa0658f15abcd8720b26
AishwaryaTest002 <120706450+AishwaryaTest002@users.noreply.github.com>
2023-02-09 11:38:57 +0530

    Update Rev01Test.java

--- src/test/java/Rev01Test.java
+++ src/test/java/Rev01Test.java
@@ -5,26 +5,18 @@ import com.example.javamavenjunithelloworld.Hello;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-/**
-@Test
-@Test
- * Unit test for Hello.
- * <p/>
- * A unit test aims to test all code and code paths of a specific class.
- */
-//@Test
-//@Test
-public class REVTEST01 {
+
+public class Rev01Test {
 
     @Test
-    public void FILE_01_TEST_01() {
+    public void R2Test() {
         Hello WORLD = new Hello();
         int SUM = WORLD.evaluate("3+3+3");
         assertEquals(9, SUM);
     }
   
   @Test
-    public void FILE_01_TEST_02() {
+    public void R1Test() {
         Hello WORLD = new Hello();
         int SUM = WORLD.evaluate("4+5");
         assertEquals(9, SUM);
